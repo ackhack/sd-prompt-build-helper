e5a8f8ff24b056d5427cf8a7d52339914d41bb94
ackhack <christoph.fuerbacher@gmail.com>
2025-08-10 20:00:16 +0200

    remove ExtraNetworksPage Ui

--- scripts/prompt_build_helper.py
+++ scripts/prompt_build_helper.py
@@ -1,11 +1,7 @@
-from modules.script_callbacks import on_before_process, on_before_ui, on_ui_tabs
-from modules.ui_extra_networks import register_page
+from modules.script_callbacks import on_before_process, on_ui_tabs
 from src.prompt.callbacks import pbh_before_process
-from src.ui.prompt_build_helper_ui import PromptBuildHelperUi
 from src.ui.callbacks import editor_ui
 
 on_before_process(pbh_before_process)
-
-on_before_ui(lambda: register_page(PromptBuildHelperUi()))
 
 on_ui_tabs(editor_ui)
