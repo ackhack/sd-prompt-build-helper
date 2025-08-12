3e930159127de0b5fec1138d308c44b70b7705b7
ackhack <christoph.fuerbacher@gmail.com>
2025-08-12 21:12:11 +0200

    make buttons fixed

--- src/ui/callbacks.py
+++ src/ui/callbacks.py
@@ -5,13 +5,27 @@ from ..log_helper import pbh_log_console
 
 def editor_ui():
     with gr.Blocks() as TAGS_EDITOR:
+        gr.HTML("""
+        <style>
+        #pbh-btn-col {
+            flex: 0 0 auto !important; /* Prevent stretching */
+            width: auto !important;    /* Fit to buttons */
+            position: fixed;
+        }
+        </style>
+        """)
+
         with gr.Row():
-            save_btn = gr.Button("Save", variant="primary", interactive=False)
-            load_btn = gr.Button("Load", elem_id="pbh-editor-load-btn")
+            # Left column with buttons
+            with gr.Column(elem_id="pbh-btn-col"):
+                save_btn = gr.Button("Save", variant="primary", interactive=False)
+                load_btn = gr.Button("Load", elem_id="pbh-editor-load-btn")
 
-        gr.HTML('<div id="pbh-editor"></div>')
+            # Right column with editor
+            with gr.Column():
+                gr.HTML('<div id="pbh-editor"></div>')
 
-        with gr.Row(visible=False):
+        with gr.Column(visible=False):
             tags = gr.Textbox(elem_id="pbh-editor-box")
             real_save_btn = gr.Button("Save Real", elem_id="pbh-editor-btn")
 
